375a91ef6940abed509a39f589ffe183e1400597
Jericho Magsino <jerichomagsino28@gmail.com>
2026-02-02 10:10:41 +0800

    I added division

---
+++ CalculatorWeb/Controllers/DivisionController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CalculatorWeb.Models;
+namespace CalculatorWeb.Controllers
+{
+    public class DivisionController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Division d1, int firstNumber, int secondNumber)
+        {
+            if (secondNumber == 0)
+            {
+                ViewBag.ErrorMessage = "Division by zero is not allowed.";
+                return View();
+            }
+
+            return View(d1);
+        }
+    }
+}
---
+++ CalculatorWeb/Models/Division.cs
@@ -0,0 +1,11 @@
+﻿namespace CalculatorWeb.Models
+{
+    public class Division
+    {
+        public int FirstNumber { get; set; }
+
+        public int SecondNumber { get; set; }
+
+        public double Quotient => (double)FirstNumber / SecondNumber;
+    }
+}
